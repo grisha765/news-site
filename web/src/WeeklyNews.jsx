f5754f20bdf6660a6e241831cc486d644ff41259
Grisha <grisha765@tuta.io>
2024-08-14 17:18:44 +0300

    [+] add weeklyNews

---
+++ web/src/WeeklyNews.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './weeklyNews.css';
+
+function WeeklyNews() {
+  return (
+    <div className="weekly-news-container">
+      <h1>Новости недели</h1>
+      <p>Здесь будут отображаться все последние новости за неделю.</p>
+      {/* Дополнительно можно добавить контент, новости, изображения и т.д. */}
+    </div>
+  );
+}
+
+export default WeeklyNews;
+
