f6c2ffc73fc4d96fb23bae762aad7e8337d57f06
Grisha <grisha765@tuta.io>
2024-08-14 17:18:58 +0300

    [+] add Anekdots

---
+++ web/src/Anekdots.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './anekdots.css';
+
+function Anekdots() {
+  return (
+    <div className="anekdots-container">
+      <h1>Анекдоты</h1>
+      <p>Здесь будут анекдоты</p>
+      {/* Дополнительно можно добавить контент, новости, изображения и т.д. */}
+    </div>
+  );
+}
+
+export default Anekdots;
