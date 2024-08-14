64eb8a1d124309879f6a19ab1b6820fdc4363d50
Grisha <grisha765@tuta.io>
2024-08-14 15:55:52 +0300

    [+] add Header

---
+++ web/src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <div className="header-container">
+      <div className="header-title">News Site</div>
+    </div>
+  );
+}
+
+export default Header;
+
