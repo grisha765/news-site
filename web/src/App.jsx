84f9776ac835aae22662a47d00fa58d24b244106
Grisha <grisha765@tuta.io>
2024-08-15 17:31:36 +0300

    [u] use HashRouter

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/index.css';
 import Header from './Header';
 import WeeklyNews from './WeeklyNews';
