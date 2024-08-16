2d8698b450ff9dda70dc878f2c6f67f384e205c6
Grisha <grisha765@tuta.io>
2024-08-16 18:27:26 +0300

    [+] init backend

---
+++ back/__main__.py
@@ -0,0 +1,13 @@
+import os, sys, uvicorn, asyncio
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from config.config import Config
+from config import logging_config
+logging = logging_config.setup_logging(__name__)
+
+async def main():
+    uvicorn.run("core.api:app", host=Config.api_ip, port=Config.api_port, reload=True)
+
+if __name__ == '__main__':
+    asyncio.run(main())
