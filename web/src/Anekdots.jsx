cd2fa3916d3e61663984c6440318600e4e30ca5d
Grisha <grisha765@tuta.io>
2024-08-21 17:56:12 +0300

    [u] import newsPosts

--- web/src/Anekdots.jsx
+++ web/src/Anekdots.jsx
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
 import './styles/anekdots.css';
 import { UserContext } from './UserContext';
+import NewsPosts from './NewsPosts';
 
 function Anekdots() {
   const { userData } = useContext(UserContext);
   const username = userData?.username || 'Аноним';
   const role = userData?.role || 'без роли';
+
+  const selectedCategories = ['anekdots'];
   return (
     <div className="anekdots-container">
       <h1>Анекдоты для {username} ({role})</h1>
-      <p>Здесь будут анекдоты</p>
-      {/* Дополнительно можно добавить контент, новости, изображения и т.д. */}
+      <NewsPosts categories={selectedCategories} />
     </div>
   );
 }
