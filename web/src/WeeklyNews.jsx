411471aa67f3fe397caf809d7252aa7ddc95c44b
Grisha <grisha765@tuta.io>
2024-08-21 17:55:58 +0300

    [u] import newsPosts

--- web/src/WeeklyNews.jsx
+++ web/src/WeeklyNews.jsx
@@ -1,16 +1,19 @@
 import React, { useContext } from 'react';
 import './styles/weeklyNews.css';
 import { UserContext } from './UserContext';
+import NewsPosts from './NewsPosts';
 
 function WeeklyNews() {
   const { userData } = useContext(UserContext);
   const username = userData?.username || 'Аноним';
   const role = userData?.role || 'без роли';
+
+  const selectedCategories = ['weekly-news'];
+
   return (
     <div className="weekly-news-container">
       <h1>Новости недели для {username} ({role})</h1>
-      <p>Здесь будут отображаться все последние новости за неделю.</p>
-      {/* Дополнительно можно добавить контент, новости, изображения и т.д. */}
+      <NewsPosts categories={selectedCategories} />
     </div>
   );
 }
