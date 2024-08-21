b2bc66097567166be8a466a4036b9dca716e42d9
Grisha <grisha765@tuta.io>
2024-08-21 18:45:09 +0300

    [u] import dotenv

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   base: '/news_site',
   plugins: [react()],
-})
+  define: {
+    'process.env': { ...process.env },
+  },
+});
+
 
