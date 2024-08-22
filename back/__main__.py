83107bd96ac568d61cd1217184ccf08b71cdfffb
Grisha <grisha765@tuta.io>
2024-08-22 19:14:58 +0300

    [u] add logic https option

--- back/__main__.py
+++ back/__main__.py
@@ -7,7 +7,22 @@ from config import logging_config
 logging = logging_config.setup_logging(__name__)
 
 def main():
-    uvicorn.run("core.api:app", host=Config.api_ip, port=Config.api_port, reload=True)
+    ssl_params = {}
+    if "example.com" not in Config.api_https:
+        parts = Config.api_https.split('/')
+        logging.info(f"use https mode: https://{parts[4]}:{str(Config.api_port)}")
+        ssl_params = {
+            'ssl_keyfile': f'{Config.api_https}/privkey.pem',
+            'ssl_certfile': f'{Config.api_https}/fullchain.pem'
+        }
+
+    uvicorn.run(
+                "core.api:app",
+                host=Config.api_ip,
+                port=Config.api_port,
+                reload=True,
+                **ssl_params
+                )
 
 if __name__ == '__main__':
     if Config.tests == "True":
