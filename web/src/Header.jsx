710ef41d61158f71048f994a139d635db1bcdb33
Grisha <grisha765@tuta.io>
2024-08-28 02:12:05 +0300

    [u] use auth in header

--- web/src/Header.jsx
+++ web/src/Header.jsx
@@ -1,44 +1,30 @@
 import { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
-import api from './api';
 import './styles/header.css';
 import CreatePost from './CreatePost';
 import { UserContext } from './UserContext';
+import Auth from './Auth'; // Импортируем новый компонент Auth
 
 function Header() {
   const [showMenu, setShowMenu] = useState(false);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const [isFrozen, setIsFrozen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showCreatePost, setShowCreatePost] = useState(false);
-  const { userData, setUserData } = useContext(UserContext);
+  const { userData } = useContext(UserContext);
 
   const handleLoginClick = () => {
     setIsFrozen(!isFrozen);
     setShowMenu(!showMenu);
   };
 
-  const handleLoginSubmit = async () => {
-    try {
-      setIsFrozen(!isFrozen);
-      const response = await api.post('/login', { username, password });
-      const user = { username: response.data.user, role: response.data.role };
-      setUserData(user);
-      setIsLoggedIn(true);
-      setShowMenu(false);
-
-    } catch (error) {
-      console.error('Ошибка входа:', error);
-      setShowMenu(false);
-    }
+  const handleLoginSuccess = (user) => {
+    setIsLoggedIn(true);
+    setShowMenu(false);
   };
 
   const handleLogout = () => {
     setIsLoggedIn(false);
-    setUserData({ username: '', role: '' });
-    setUsername('');
-    setPassword('');
+    setShowMenu(false);
   };
 
   const handleCreatePostClick = () => {
@@ -49,69 +35,53 @@ function Header() {
     setShowCreatePost(false); // Закрываем модальное окно
   };
 
+  const handleCloseMenu = () => {
+    setIsFrozen(false);
+    setShowMenu(false);
+  };
+
   return (
-    <div className="header-container">
-      <Link to="/" className="header-title">Новости</Link>
-      <div className="button-container">
-        <button className="button" onClick={() => window.location.href = '#'}>
-          Главная
-        </button>
-        <button className="button" onClick={() => window.location.href = '#/weekly-news'}>
-          Новости недели
-        </button>
-        <button className="button" onClick={() => window.location.href = '#/anekdots'}>
-          Анекдоты
-        </button>
-      </div>
-      <div className="auth-container">
-        {isLoggedIn && userData.role === 'admin' && (
-          <button className="button" onClick={handleCreatePostClick}>
-            Создать Пост
+    <>
+      <div className="header-container">
+        <Link to="/" className="header-title">Новости</Link>
+        <div className="button-container">
+          <button className="button" onClick={() => window.location.href = '#'}>
+            Главная
           </button>
-        )}
-        {isLoggedIn ? (
-          <div className="user-menu">
-            <button className={`login-button ${isFrozen ? 'frozen' : ''}`} onClick={handleLoginClick}>
-              {showMenu ? 'Закрыть' : userData.username}
+          <button className="button" onClick={() => window.location.href = '#/weekly-news'}>
+            Новости недели
+          </button>
+          <button className="button" onClick={() => window.location.href = '#/anekdots'}>
+            Анекдоты
+          </button>
+        </div>
+        <div className="auth-container">
+          {isLoggedIn && userData.role === 'admin' && (
+            <button className="button" onClick={handleCreatePostClick}>
+              Создать Пост
             </button>
-            {showMenu && (
-              <div className="dropdown-menu">
-                <p>User: {userData.username}</p>
-                <p>Role: {userData.role}</p>
-                <button onClick={handleLogout}>Выход</button>
-              </div>
-            )}
-          </div>
-        ) : (
-          <>
-            <button className={`login-button ${isFrozen ? 'frozen' : ''}`} onClick={handleLoginClick}>
-              Вход
-            </button>
-            {showMenu && (
-              <div className="login-menu">
-                <input
-                  type="text"
-                  placeholder="Логин"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                  type="password"
-                  placeholder="Пароль"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <button onClick={handleLoginSubmit}>Вход</button>
-              </div>
-            )}
-          </>
-        )}
+          )}
+          <button className={`button ${isFrozen ? 'frozen' : ''}`} onClick={handleLoginClick}>
+            {isLoggedIn ? userData.username : 'Вход'}
+          </button>
+        </div>
       </div>
+
+      {showMenu && (
+        <div className="auth-popup">
+          <Auth
+            isLoggedIn={isLoggedIn}
+            onLoginSuccess={handleLoginSuccess}
+            onLogout={handleLogout}
+            onCloseMenu={handleCloseMenu}
+          />
+        </div>
+      )}
+      
       {showCreatePost && <CreatePost onClose={handleCloseCreatePost} />}
-    </div>
+    </>
   );
 }
 
 export default Header;
-
 
