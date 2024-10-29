1ed1c1bc5c664d7f0f6f453ce4a6b3d02f366278
Grisha <grisha765@tuta.io>
2024-10-29 22:24:57 +0300

    [-] delete dotenv

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default defineConfig({
   base: '/news-site',
   plugins: [react()],
-  define: {
-    'process.env': { ...process.env },
-  },
 });
-
-
