25ae8b6b76aec4b1f05c305b1b50917eea927aaa
Grisha <grisha765@tuta.io>
2024-10-29 22:24:15 +0300

    [f] import meta env

--- web/src/api.js
+++ web/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.VITE_IP_ADDRESS || 'http://0.0.0.0:8000';
+const baseURL = import.meta.env.VITE_IP_ADDRESS || 'http://0.0.0.0:8000';
 
 const api = axios.create({
   baseURL: baseURL,
